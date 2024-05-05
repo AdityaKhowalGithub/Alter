93458fd84e326186aa2a64eb82baa8638be110cf
Shiina Sugioka <shiina12@uw.edu>
2024-05-05 13:21:21 -0700

    added front end to input page

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,22 +1,34 @@
 import axios from 'axios';
 import './App.css';
 
-const apiCall = () => {
-  axios.get('http://localhost:8080').then((data) => {
-    console.log(data)
-  })
+const testApiCall = () => {
+    axios.get('http://localhost:8080').then((data) => {
+        console.log(data)
+    });
 }
 
 const App = () => {
-  return (
-      <div className="App">
-        <header className="App-header">
+    return (
+        <div className="App">
+            <div className={"input-content-wrapper"}>
+                <p className={"page-header"}>Generate Alt text for website</p>
 
-          <button onClick={apiCall}>Testing Make API Call</button>
+                <div className={"input-and-submit"}>
+                    <div className="user-input-field">
+                        <input type="input" className="user-input" placeholder="Add your URL here" name="Website URL"
+                               required/>
+                        <label htmlFor={"Website URL"} className="user-input-label">Website URL</label>
+                    </div>
 
-        </header>
-      </div>
-  );
+                    <button id={"submit-user-input-button"} role={"button"}>Submit</button>
+                </div>
+
+            </div>
+
+            {/*<button onClick={testApiCall}>Testing Make API Call</button>*/}
+
+        </div>
+    );
 }
 
-export default App
+export default App;
