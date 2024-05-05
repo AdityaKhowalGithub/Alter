c48d33cdb40a5ea1ffd9ca2be0615a9d395d9302
Shiina Sugioka <shiina12@uw.edu>
2024-05-05 13:52:56 -0700

    added url check

--- src/client/src/App.js
+++ src/client/src/App.js
@@ -1,5 +1,7 @@
 import axios from 'axios';
 import './App.css';
+import validator from 'validator/es';
+import { useState } from 'react';
 
 const testApiCall = () => {
     axios.get('http://localhost:8080').then((data) => {
@@ -8,19 +10,53 @@ const testApiCall = () => {
 }
 
 const App = () => {
+    const [errorMessage, setErrorMessage] = useState('');
+
+    const validate = (value) => {
+        if (!validator.isURL(value)) {
+            setErrorMessage('Invalid URL');
+            return false;
+        } else {
+            setErrorMessage('');
+            return true;
+        }
+    }
+
+    const submitURL = (e) => {
+        e.preventDefault();
+        const inputElement = document.getElementById('user-input');
+        const inputValue = inputElement.value;
+
+        if (validate(inputValue)) {
+            axios.get(inputValue)
+                .then((response) => {
+                    console.log(response);
+                })
+                .catch((error) => {
+                    console.error(error);
+                });
+        }
+    }
+
+    const clearErrorMessage = () => {
+        setErrorMessage('');
+    }
+
     return (
         <div className="App">
             <div className={"input-content-wrapper"}>
                 <p className={"page-header"}>Generate Alt text for website</p>
 
                 <div className={"input-and-submit"}>
-                    <div className="user-input-field">
-                        <input type="input" className="user-input" placeholder="Add your URL here" name="Website URL"
-                               required/>
-                        <label htmlFor={"Website URL"} className="user-input-label">Website URL</label>
+                    <div className={"user-input-field"}>
+                        <input type="input" id={"user-input"} placeholder={"Add your URL here"} name={"Website URL"}
+                               required onChange={clearErrorMessage}/>
+                        <label htmlFor={"Website URL"} className={"user-input-label"}>Website URL</label>
                     </div>
 
-                    <button id={"submit-user-input-button"} role={"button"}>Submit</button>
+                    <button id={"submit-user-input-button"} role={"button"} onClick={submitURL}>Submit</button>
+                    {errorMessage && <span id={"url-error"}>{errorMessage}</span>}
+                    {/*  the fact that this moves bothers me so much  */}
                 </div>
 
             </div>
